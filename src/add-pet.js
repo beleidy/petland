31d4dc0c57d9238f995a2b4f32eaaf7831cd7e63
beleidy <a.elbeleidy@gmail.com>
2017-03-16 08:31:55 +0000

    disable eslint bitwise warning

--- src/add-pet.js
+++ src/add-pet.js
@@ -118,6 +118,7 @@ class AddPet extends Component{
 
         //Generate a random file name
         const fileName = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            // eslint-disable-next-line
             var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
         });
