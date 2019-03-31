a2f42628af93acd73d2fb41d4b2a71c9696a2935
Amr Elbeleidy <amr@elbeleidy.me>
2019-03-31 20:46:08 +0100

    adds dynamic configuration of app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import firebase from "firebase/app";
 
+import firebaseConfig from "../firebase-js-config.json";
 import "./css/index.css";
 import PetPage from "./pet";
 import Welcome from "./welcome";
@@ -10,23 +11,16 @@ import AddPet from "./add-pet";
 import MainLayout from "./main-layout";
 
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDXSjkM4Jnc9KcU5ERfdTkrHRiy5Xh-EoU",
-    authDomain: "petland-dev.firebaseapp.com",
-    databaseURL: "https://petland-dev.firebaseio.com",
-    projectId: "petland-dev",
-    storageBucket: "petland-dev.appspot.com",
-    messagingSenderId: "738118892814"
-};
+var config = firebaseConfig.result;
 firebase.initializeApp(config);
 
 ReactDOM.render(
-    <Router>
-        <MainLayout>
-            <Route exact path="/" component={Welcome} />
-            <Route path="/pet/:id" component={PetPage} />
-            <Route path="/add-pet" component={AddPet} />
-        </MainLayout>
-    </Router>,
-    document.getElementById("root")
+  <Router>
+    <MainLayout>
+      <Route exact path="/" component={Welcome} />
+      <Route path="/pet/:id" component={PetPage} />
+      <Route path="/add-pet" component={AddPet} />
+    </MainLayout>
+  </Router>,
+  document.getElementById("root")
 );
