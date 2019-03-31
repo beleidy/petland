905fc3f1181d975d3bfeba01479a3fab9f57f8cb
Amr Elbeleidy <amr@elbeleidy.me>
2019-03-31 21:16:06 +0100

    moves firebase js configuratio inside src directory

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import firebase from "firebase/app";
 
-import firebaseConfig from "../firebase-js-config.json";
+import firebaseConfig from "./firebase-js-config.json";
 import "./css/index.css";
 import PetPage from "./pet";
 import Welcome from "./welcome";
