f17a61d3e3e3206a9459e6ea7eba61368f344128
Amr Elbeleidy <amr@elbeleidy.me>
2019-04-02 10:07:49 +0100

    fixes pet card

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,24 +3,6 @@ import { Link } from "react-router-dom";
 import firebase from "firebase/app";
 import "firebase/database";
 
-function PetCard(props) {
-  return (
-    <Link
-      className="w-64 h-48 mx-5 my-10 no-underline"
-      to={"/pet/" + props.petId}
-    >
-      <div
-        className="w-full h-full flex items-end rounded-lg bg-cover"
-        style={{ backgroundImage: `url(${props.petImageURL})` }}
-      >
-        <div className="w-full text-center text-content-color bg-grey-lighter">
-          {props.petName}
-        </div>
-      </div>
-    </Link>
-  );
-}
-
 function HomePage(props) {
   const [pets, setPets] = useState([]);
 
@@ -41,7 +23,7 @@ function HomePage(props) {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-wrap justify-around">
+    <div className="mx-auto flex flex-wrap justify-around">
       {pets
         .slice()
         .reverse()
@@ -54,6 +36,20 @@ function HomePage(props) {
           />
         ))}
     </div>
+  );
+}
+
+function PetCard(props) {
+  return (
+    <Link
+      className="w-64 h-48 mx-5 my-10 no-underline flex items-end rounded-lg bg-cover"
+      style={{ backgroundImage: `url(${props.petImageURL})` }}
+      to={"/pet/" + props.petId}
+    >
+      <span className="w-full text-center text-content-color font-content bg-my-blue rounded-b py-1">
+        {props.petName}
+      </span>
+    </Link>
   );
 }
 
