8798e26cd5d5c501edb474c0c66df2b4d777bf99
Amr Elbeleidy <amr@elbeleidy.me>
2019-04-04 21:33:49 +0100

    refactors add pet submission

--- src/AddPet.js
+++ src/AddPet.js
@@ -14,9 +14,7 @@ function AddPet(props) {
   const [uploadFile, setUploadFile] = useState(null);
 
   useEffect(() => {
-    // Set observer on user state
     const unsubscribe = firebase.auth().onAuthStateChanged(user => {
-      //If the user is signed in, set isSigned in to true, otherwise set it false
       if (user) {
         setIsSignedIn(true);
       } else {
@@ -26,102 +24,51 @@ function AddPet(props) {
     return unsubscribe;
   }, []);
 
-  const handleSubmit = event => {
-    /********************************************************************************************
-        This function handles the submission of the form, general logic is as follows:
-        1. Check whether there is a user or not
-        If not, abort form submission, if there is a user:
-            2.Check if image will be a file upload or image link,
-                2.a If a url, fetch, if error, abort submission, if not, store image blob
-                2.b If a file, check file exists, if error, abort submission, if not, store image blob
-            3. Generate random file name, Upload blob, if error, abort submission, if not - get download url
-            4. create a new key, db object and push to db, Navigate to new pet page
-        Note: steps 3 and 4 are described in a function that is only invoked once the image blob is ready to avoid async issues.
-        **********************************************************************************************/
-
+  const handleSubmit = async event => {
     event.preventDefault();
 
-    // 1. If there is no user signed in
     if (!isSignedIn) {
-      //Tell the user they are not signed in, and abort form submission
       setErrorMessage("Please sign in to add your pet");
       return null;
     }
-    // 1. If the user is signed in
-    else {
-      // 2a. If image is a link
-      if (!isFileUpload) {
-        //Replace http with https in imageURL
-        const secureImage = imageURL.replace("http:", "https:");
 
-        // Use fetch api to check that the url is an image
-        fetch(secureImage)
-          .then(response => {
-            // Check if link is of type image
-            const linkIsImage = response.headers
-              .get("content-type")
-              .startsWith("image");
-            if (linkIsImage) {
-              return response.blob();
-            } else {
-              setErrorMessage(
-                "Sorry, your link does not point to an image, please check and try again."
-              );
-              return null;
-            }
-          })
-          .then(imageBlob => {
-            // If the image blob is null, return null
-            if (!imageBlob) return null;
-            startUpload(imageBlob);
-          })
-          .catch(error => {
-            setErrorMessage(
-              "Sorry, your link did not work - are you sure it points to an image?"
-            );
-            return null;
-          });
+    let imageBlob;
+
+    if (isFileUpload) {
+      if (uploadFile.type.startsWith("image")) {
+        imageBlob = uploadFile;
       } else {
-        // 2b. If Image is a file upload
-        const fileIsImage = uploadFile.type.startsWith("image");
-        if (fileIsImage) {
-          // Asign the file object to imageBlob variable
-          startUpload(uploadFile);
-        } else {
-          // Display error and abort
-          setErrorMessage(
-            "The file you selected is not an image. Please select another one."
-          );
-          return null;
-        }
+        setErrorMessage(
+          "The file you selected is not an image. Please select another one."
+        );
+        return null;
       }
+    } else {
+      const secureImage = imageURL.replace("http:", "https:");
+      const response = await fetch(secureImage);
+
+      if (!response.headers.get("content-type").startsWith("image")) {
+        setErrorMessage(
+          "Sorry, your link does not point to an image, please check and try again."
+        );
+        return null;
+      }
+      imageBlob = await response.blob();
     }
-  };
 
-  const startUpload = imageBlob => {
-    // 3. Now we have an image blob and it is time to upload it
-
-    // Generate a random file name
     const fileName = uuidv4();
-
-    // Create a storage bucket root refrence
     const storageRootRef = firebase.storage().ref();
-
-    // Get user id from firebase
     const ownerId = firebase.auth().currentUser.uid;
+    const storageFileRef = storageRootRef.child(`${ownerId}/${fileName}`);
 
-    // Create a firebase storage bucket file refrence using user id and file name
-    const storageFileRef = storageRootRef.child(ownerId + "/" + fileName);
+    const uploadTask = storageFileRef.put(imageBlob);
 
-    // Ask firebase to upload the file
-    var uploadTask = storageFileRef.put(imageBlob);
-
-    // Track file upload status in console, can be used later to animate a progress bar
     const cancelUploadListner = uploadTask.on(
       "state_changed",
       snapshot => {
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`Upload is ${progress}% done`);
       },
       error => {
         console.debug("Upload Error");
@@ -130,38 +77,39 @@ function AddPet(props) {
         );
         return null;
       },
-      () => {
+      async () => {
         // When upload is done and successful
         console.debug("Upload Success!");
-        storageFileRef.getDownloadURL().then(downloadURL => {
-          console.debug(downloadURL);
-          // 4. Create the db object, push to database and navigate user to new pet page
 
-          // Create object to be passed to firebase
-          const dbNewPet = {
-            name: petName,
-            imageURL: downloadURL,
-            ownerId: ownerId
-          };
-          //Push new pet information to firebase
-          var newKey = firebase
-            .database()
-            .ref()
-            .child("pets")
-            .push().key;
-          const updates = {};
-          updates["/pets/" + newKey] = dbNewPet;
-          firebase
-            .database()
-            .ref()
-            .update(updates);
-          // Reset state
-          setPetName("");
-          setImageURL("");
-          // Navigate user back to welcome page
-          props.history.push("/pet/" + newKey);
-          cancelUploadListner();
-        });
+        const downloadURL = await storageFileRef.getDownloadURL();
+
+        const dbNewPet = {
+          name: petName,
+          imageURL: downloadURL,
+          ownerId: ownerId
+        };
+        //Push new pet information to firebase
+        const newKey = firebase
+          .database()
+          .ref()
+          .child("pets")
+          .push().key;
+
+        const updates = {};
+        updates[`/pets/${newKey}`] = dbNewPet;
+        firebase
+          .database()
+          .ref()
+          .update(updates);
+
+        // Reset state
+        setPetName("");
+        setImageURL("");
+
+        cancelUploadListner();
+
+        // Navigate user back to welcome page
+        props.history.push(`/pet/${newKey}`);
       }
     );
   };
